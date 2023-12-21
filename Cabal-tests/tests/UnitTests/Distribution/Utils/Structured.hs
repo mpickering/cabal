7405a8abfbde7023fadfcc977cf4151b61738170
sheaf <sam.derbyshire@gmail.com>
2023-12-21 14:15:51 +0100

    Split up LocalBuildInfo
    The aim of this commit is to modularise LocalBuildInfo to better
    reflect implicit invariants.
    
    The top-level split
    
      data LocalBuildInfo =
        NewLocalBuildInfo
          { localBuildDescr  :: LocalBuildDescr
          , localBuildConfig :: LocalBuildConfig
          }
    
    reflects which part of a LocalBuildInfo are set in stone by Cabal
    (the LocalBuildDescr), while LocalBuildConfig contains options that
    can be controlled/modified by the user in some way.
    
    The split
    
      data LocalBuildDescr =
        LocalBuildDescr
        { packageBuildDescr :: PackageBuildDescr
        , componentBuildDescr :: ComponentBuildDescr
        }
    
    reflects that some parts of the information determined at configuration
    time resides at the package level, while other pieces of information
    are pertinent to individual components.
    
    Finally the structure of LocalBuildConfig is aimed to reduce code
    duplication between the Cabal library and cabal-install.

--- Cabal-tests/tests/UnitTests/Distribution/Utils/Structured.hs
+++ Cabal-tests/tests/UnitTests/Distribution/Utils/Structured.hs
@@ -33,15 +33,15 @@ md5Check proxy md5Int = structureHash proxy @?= md5FromInteger md5Int
 md5CheckGenericPackageDescription :: Proxy GenericPackageDescription -> Assertion
 md5CheckGenericPackageDescription proxy = md5Check proxy
 #if MIN_VERSION_base(4,19,0)
-    0xc638caeb7531f107f64d12773f9430d0
+    0x7e730e85b1462b672aed0e00c4622d5e
 #else
-    0x7a231bff7bb37049ec7f2ebfd98d3243
+    0x93fea18754116735ad62894838f8c891
 #endif
 
 md5CheckLocalBuildInfo :: Proxy LocalBuildInfo -> Assertion
 md5CheckLocalBuildInfo proxy = md5Check proxy
 #if MIN_VERSION_base(4,19,0)
-    0x023b3cd1665b2acdedf72d231c96336b
+    0x92782413689aba84f1ae8a71e0ff4475
 #else
-    0xc6c0cc122cc60ce7943764cbaaacdc2d
+    0xfa44477c60b169f306656c4df793c2c5
 #endif
