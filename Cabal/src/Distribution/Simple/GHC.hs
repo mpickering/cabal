0068090245df8322e5395ffa422e4cebfb40ef9b
Ian-Woo Kim <ianwookim@gmail.com>
2024-02-14 10:15:16 -0800

    Re-revert: Revert #3639 (Don't pass -package-db and -package flags to --abi-hash) (#9384) for hadrian built with GHC 9.4 and Cabal HEAD to successfully build GHC.

--- Cabal/src/Distribution/Simple/GHC.hs
+++ Cabal/src/Distribution/Simple/GHC.hs
@@ -676,12 +676,20 @@ libAbiHash verbosity _pkg_descr lbi lib clbi = do
     libBi = libBuildInfo lib
     comp = compiler lbi
     platform = hostPlatform lbi
-    vanillaArgs =
+    vanillaArgs0 =
       (Internal.componentGhcOptions verbosity lbi libBi clbi (componentBuildDir lbi clbi))
         `mappend` mempty
           { ghcOptMode = toFlag GhcModeAbiHash
           , ghcOptInputModules = toNubListR $ exposedModules lib
           }
+    vanillaArgs =
+      -- Package DBs unnecessary, and break ghc-cabal. See #3633
+      -- BUT, put at least the global database so that 7.4 doesn't
+      -- break.
+      vanillaArgs0
+        { ghcOptPackageDBs = [GlobalPackageDB]
+        , ghcOptPackages = mempty
+        }
     sharedArgs =
       vanillaArgs
         `mappend` mempty
